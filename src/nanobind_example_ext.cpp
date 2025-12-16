3e423f1c0b556a04daaa1738871d8f7cca791cb7
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-12-16 15:26:26 +0100

    Add proper type hint support

--- src/nanobind_example_ext.cpp
+++ src/nanobind_example_ext.cpp
@@ -4,7 +4,9 @@ namespace nb = nanobind;
 
 using namespace nb::literals;
 
+static int add(int a, int b) { return a + b; }
+
 NB_MODULE(nanobind_example_ext, m) {
-    m.doc() = "This is a \"hello world\" example with nanobind";
-    m.def("add", [](int a, int b) { return a + b; }, "a"_a, "b"_a);
+  m.doc() = "This is a \"hello world\" example with nanobind";
+  m.def("add", &add, "a"_a, "b"_a, R"(Add two integers together.)");
 }
