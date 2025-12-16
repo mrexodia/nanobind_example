c3268c5d4d3c828c711de61c4655b121ea3bde13
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-12-16 17:51:04 +0100

    Change module to be top-level package

--- src/nanobind_example.cpp
+++ src/nanobind_example.cpp
@@ -6,7 +6,7 @@ using namespace nb::literals;
 
 static int add(int a, int b) { return a + b; }
 
-NB_MODULE(nanobind_example_ext, m) {
+NB_MODULE(nanobind_example, m) {
   m.doc() = "This is a \"hello world\" example with nanobind";
   m.def("add", &add, "a"_a, "b"_a, R"(Add two integers together.)");
 }
--- src/nanobind_example/__init__.py
+++
@@ -1,6 +0,0 @@
-# NOTE: we use an absolute import here to get the type checking to work properly.
-# Additionally we re-export the imported symbols to make ruff happy.
-from nanobind_example.nanobind_example_ext import (
-    add as add,
-    __doc__ as __doc__,
-)
